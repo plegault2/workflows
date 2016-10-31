5898ca932b5b3858beb6356e2dc75a5b0c30162d
Phil Legault <legaulph@optonline.net>
2016-10-31 10:35:30 -0400

    Added browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    browserify = require('gulp-browserify');
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
 'components/scripts/rclick.js',
@@ -19,6 +20,7 @@ gulp.task('coffee', function(){
 gulp.task('js', function(){
 	gulp.src(jsSources)
 	.pipe(concat('script.js'))
+	.pipe(browserify())
 	.pipe(gulp.dest('builds/development/js'))
 
 });
